41861c7e260c75e6ada6fffa63993af6021f02a6
Bhanu Prabhath <70201186+bhanu7410@users.noreply.github.com>
2025-07-21 13:27:39 +0530

    got a direction

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,44 @@
 import { useState } from "react";
+
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import linkedinLogo from "./assets/LinkedIn Logo Icon.svg";
+import githubLogo from "./assets/github-mark-white.svg";
+
 import "./App.css";
 
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<div className="font-monaco h-screen w-screen text-black lg:px-20 dark:bg-[rgba(17,17,17,1)] dark:text-white">
+		<div className="font-monaco h-screen w-screen bg-[rgba(17,17,17,1)] text-white selection:bg-orange-400 selection:text-white lg:px-20">
 			{/* <Navbar /> */}
 			<Card>
 				<div className="flex h-full items-center justify-start">
-					<div className="inline-block text-4xl">Bhanu Prabhath</div>
-					<div className="inline-block text-2xl">
-						<img src={viteLogo} alt="" />
+					<div className="inline-block grow text-4xl">
+						Bhanu Prabhath
+					</div>
+					<div className="group mr-2 inline-block size-12 flex-wrap rounded-xl border-2 border-stone-600/50 p-2 hover:cursor-pointer hover:border-orange-500 hover:bg-orange-500/15">
+						<img
+							src={githubLogo}
+							alt=""
+							className="grayscale transition-transform duration-300 group-hover:-translate-y-0.5 group-hover:grayscale-0"
+						/>
+					</div>
+					<div className="group mr-2 inline-block size-12 flex-wrap rounded-xl border-2 border-stone-600/50 p-2 hover:cursor-pointer hover:border-orange-500 hover:bg-orange-500/15">
+						<img
+							src={linkedinLogo}
+							alt=""
+							className="grayscale transition-transform duration-300 group-hover:-translate-y-0.5 group-hover:grayscale-0"
+						/>
+					</div>
+					<div className="group mr-2 inline-block size-12 flex-wrap rounded-xl border-2 border-stone-600/50 p-2 hover:cursor-pointer hover:border-orange-500 hover:bg-orange-500/15">
+						<img
+							src={githubLogo}
+							alt=""
+							className="transition-transform duration-300 group-hover:-translate-y-0.5"
+						/>
 					</div>
 				</div>
 			</Card>
